080e3430f9c7f71dffecc2f5c565222579a1a259
dummy <dummy@example.com>
2026-06-16 20:36:02 +0900

    ゲームパッド設定 修正

--- main.js
+++ main.js
@@ -22,28 +22,28 @@ window.onload = func001;
     if (gp) {
         // アナログスティック
         // 左右上下
-        gamepad_key_list[37] = gp.axes[0] < -0.5 ? 1 : 0;
-        gamepad_key_list[39] = gp.axes[0] >  0.5 ? 1 : 0;
-        gamepad_key_list[38] = gp.axes[1] < -0.5 ? 1 : 0;
-        gamepad_key_list[40] = gp.axes[1] >  0.5 ? 1 : 0;
+        gamepad_key_list[var_647] = gp.axes[0] < -0.5 ? 1 : 0;
+        gamepad_key_list[var_648] = gp.axes[0] >  0.5 ? 1 : 0;
+        gamepad_key_list[var_649] = gp.axes[1] < -0.5 ? 1 : 0;
+        gamepad_key_list[var_650] = gp.axes[1] >  0.5 ? 1 : 0;
 
         // 注意：gp.buttonsのindexの割り当ては機種依存
         // 各自コードを書き換えてください。
 
         // Z 攻撃・選択
-        gamepad_key_list[90] = gp.buttons[1]?.pressed ? 1 : 0;
+        gamepad_key_list[var_655] = gp.buttons[1]?.pressed ? 1 : 0;
         // X キャンセル
-        gamepad_key_list[88] = gp.buttons[2]?.pressed ? 1 : 0;
+        gamepad_key_list[var_656] = gp.buttons[2]?.pressed ? 1 : 0;
         // A メニュー
-        gamepad_key_list[65] = gp.buttons[0]?.pressed ? 1 : 0;
+        gamepad_key_list[var_657] = gp.buttons[0]?.pressed ? 1 : 0;
         // Space 地図
         gamepad_key_list[32] = gp.buttons[7]?.pressed ? 1 : 0;
         // C 方向
-        gamepad_key_list[67] = gp.buttons[3]?.pressed ? 1 : 0;
+        gamepad_key_list[var_660] = gp.buttons[3]?.pressed ? 1 : 0;
         // Shift 斜め移動補助
-        gamepad_key_list[16] = gp.buttons[5]?.pressed ? 1 : 0;
+        gamepad_key_list[var_659] = gp.buttons[5]?.pressed ? 1 : 0;
         // S 射撃
-        gamepad_key_list[83] = gp.buttons[4]?.pressed ? 1 : 0;
+        gamepad_key_list[var_658] = gp.buttons[4]?.pressed ? 1 : 0;
     }
     requestAnimationFrame(updateGamepad);
 })();
